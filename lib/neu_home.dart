58b2e1f53f74765d1c4c623f76e91b54a26988d9
maxgdev <max@maxgdev.net>
2021-03-03 12:48:45 +0000

    Added unstyled Slider and player controls

--- lib/neu_home.dart
+++ lib/neu_home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class NeuHome extends StatefulWidget {
   NeuHome({Key key, this.title}) : super(key: key);
 
@@ -11,15 +10,13 @@ class NeuHome extends StatefulWidget {
 }
 
 class _NeuHomeState extends State<NeuHome> {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Padding(
+        body: Container(
+      child: Column(
+        children: [
+          Padding(
               padding: EdgeInsets.all(5),
               child: Row(
                 children: [
@@ -29,63 +26,88 @@ class _NeuHomeState extends State<NeuHome> {
                   Spacer(),
                   Icon(Icons.arrow_forward)
                 ],
-              )
-              ),
-            SizedBox(height: 10,),
-            Container(
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle
-              ),
-              child: Padding(
-                padding: EdgeInsets.all(5),
-                child: Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage("assets/images/burna.png"),
-                      fit: BoxFit.cover,
-                    ),
+              )),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(shape: BoxShape.circle),
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/burna.png"),
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
             ),
-            SizedBox(height: 10,),
-            RichText(
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          RichText(
               text: TextSpan(
-                text: "Ye Yo",
-                style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600,
-                ),
-              )
-              ),
-              SizedBox(height: 10,), // Calculate later
-              Text(
-                "Burna Boy",
-                style: TextStyle(
-                fontSize: 20,
-                color: Colors.black,
-                fontWeight: FontWeight.w400,
-                ),
-              ),
-              SizedBox(height: 10,), // Calculate later
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: Row(
-                  children: [
-                    Text("01:35"),
-                    Spacer(),
-                    Text("3:50"),
-                  ],
-                ),
-              ),
+            text: "Ye Yo",
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+            ),
+          )),
+          SizedBox(
+            height: 10,
+          ), // Calculate later
+          Text(
+            "Burna Boy",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ), // Calculate later
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              children: [
+                Text("01:35"),
+                Spacer(),
+                Text("3:50"),
+              ],
+            ),
+          ),
+          Slider(
+            value: 2,
+            min: 0,
+            max: 100,
+            // divisions: 5,
+            onChanged: (v) => {},
 
-          ],
-        ),
-          
-      ));
+          ),
+                    SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Icon(Icons.arrow_back_rounded),
+                Spacer(),
+                Icon(Icons.play_arrow_rounded),
+                Spacer(),
+                Icon(Icons.arrow_forward_rounded),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
